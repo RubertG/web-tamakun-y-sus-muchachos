a247d6fa0ec84b80e8814d9d3e9b32151bcc4f6e
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-08 20:29:03 -0500

    fix: parser prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,62 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import importHelpers from 'eslint-plugin-import-helpers'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: ['**/node_modules/*', '**/out/*', '**/.next/*', '**/coverage', 'src/styles/globals.css']
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    plugins: {
+      'import-helpers': importHelpers,
+      '@typescript-eslint': typescriptEslint
+    },
+    rules: {
+      'newline-before-return': 2,
+      'react/prop-types': 0,
+      'react/react-in-jsx-scope': 0,
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          newlinesBetween: 'always',
+          groups: [['/^next/', 'module'], '/^@/styles/', '/^@/components/', '/^@/lib/', ['parent', 'sibling', 'index']],
+          alphabetize: {
+            order: 'asc',
+            ignoreCase: true
+          }
+        }
+      ]
+      // '@typescript-eslint/no-unused-vars': [
+      //   2,
+      //   {
+      //     argsIgnorePattern: '^_',
+      //   },
+      // ],
+      // 'no-console': [
+      //   2,
+      //   {
+      //     allow: ['warn', 'error'],
+      //   },
+      // ],
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
