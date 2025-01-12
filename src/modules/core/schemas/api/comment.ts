8f0e419bac9e87530bd3adfb25cf414ff3ee8f62
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-12 16:39:25 -0500

    feat: add Zod schema for comment insertion validation

---
+++ src/modules/core/schemas/api/comment.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+import { CommentInsert } from '../../interfaces/db/db'
+
+const commentInsertSchemaDefinition: Record<keyof CommentInsert, z.ZodTypeAny> = {
+  approved: z.boolean().optional(),
+  comment: z.string().min(1).max(500),
+  user_id: z.string().uuid().optional(),
+  created_at: z.string().optional(),
+  id: z.string().uuid().optional()
+}
+
+export const CommentInsertSchema = z.object(commentInsertSchemaDefinition)
