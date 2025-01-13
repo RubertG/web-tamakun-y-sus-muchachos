1992017e05c773f41aafbedd1b08ca4036b93f64
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-13 12:27:49 -0500

    feat: add Event type definition for event handling

--- src/modules/core/interfaces/db/db.d.ts
+++ src/modules/core/interfaces/db/db.d.ts
@@ -5,3 +5,10 @@ export type UserInsert = Database['public']['Tables']['users']['Insert']
 
 export type Comment = Database['public']['Tables']['comments']['Row']
 export type CommentInsert = Database['public']['Tables']['comments']['Insert']
+
+export type Event = {
+  id: string
+  date: string
+  title: string
+  description: string
+}
