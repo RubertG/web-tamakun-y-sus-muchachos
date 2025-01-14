630dcb708e9d59cb166fa968f6bb660f179fdea4
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-14 18:06:58 -0500

    feat: add z-index to NavItem component for improved layering

--- src/modules/core/components/nav/nav-item.tsx
+++ src/modules/core/components/nav/nav-item.tsx
@@ -8,7 +8,7 @@ interface Props extends NavLinkType {
 
 const NavItem = ({ children, path, label }: Props) => {
   return (
-    <Link className="group relative inline-flex h-fit w-fit font-medium" href={path}>
+    <Link className="group relative z-10 inline-flex h-fit w-fit font-medium" href={path}>
       <div className="bg-gradient-primary absolute -left-[1px] -top-[1px] -z-10 hidden h-[calc(100%+2px)] w-[calc(100%+2px)] rounded-[10px] border-solid opacity-0 transition-all group-hover:opacity-100 md:inline-block" />
       <p className="md:rounded-[10px] md:bg-black md:px-[14px] md:py-[9px]">{children || label}</p>
     </Link>
