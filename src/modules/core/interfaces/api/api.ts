b7494b094b00604fda1535a79a7569fe4c45ae75
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-14 18:07:06 -0500

    feat: add CommentResponse interface to include user information in API responses

--- src/modules/core/interfaces/api/api.ts
+++ src/modules/core/interfaces/api/api.ts
@@ -1,6 +1,8 @@
 import { PostgrestError } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
+import { Comment } from '../db/db'
+
 export interface Response<T = unknown> {
   message: string
   data?: T | null
@@ -8,3 +10,8 @@ export interface Response<T = unknown> {
 }
 
 export type RouteResponse<T = unknown> = NextResponse<Response<T>>
+
+export interface CommentResponse extends Comment {
+  user_name: string
+  profile_picture: string
+}
