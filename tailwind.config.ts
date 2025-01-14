3e8c9a4830261543a77b7164f24403f44fc81022
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-14 15:52:31 -0500

    feat: add new theme settings in tailwind.config.ts, including gradients and custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,29 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-primary': 'linear-gradient(100.654deg, #E50101 20%, #1C53F9 80%);',
+        'gradient-text': 'linear-gradient(97.176deg, #E90303 29%, #1C53F9 70%);'
+      },
+      boxShadow: {
+        'button-secondary': '-5px 0px 15px rgb(229, 1, 1, 0.8), 5px 0px 15px rgb(21, 68, 208, 1)'
+      },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        'primary-50': '#ff5552',
+        'primary-100': '#ff2924',
+        'primary-200': '#e50101',
+        'primary-300': '#c3140f',
+        'bg-dark': '#060606',
+        'bg-muted': '#0a0909',
+        'bg-accent': '#302f32',
+        'accent-100': '#1c53f9',
+        'accent-200': '#1544d0',
+        'accent-300': '#1339ac',
+        'text-primary': '#ffffff',
+        'text-secondary': '#c6c6c6',
+        'text-muted': '#939393',
+        'text-inverse': '#1f1f1f',
+        'border-default': '#1a1a1a'
       }
     }
   },
