1254ccd6a2d67c387e5c3515846173c301e591de
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-15 12:14:43 -0500

    feat: add isActive prop to NavItem component for active link styling

--- src/modules/core/components/nav/nav-item.tsx
+++ src/modules/core/components/nav/nav-item.tsx
@@ -1,15 +1,25 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 
 import { NavLinkType } from '../../interfaces/nav/nav'
 
 interface Props extends NavLinkType {
-  children: React.ReactNode
+  children?: React.ReactNode
+  isActive?: boolean
 }
 
-const NavItem = ({ children, path, label }: Props) => {
+const NavItem = ({ children, path, label, isActive }: Props) => {
   return (
-    <Link className="group relative z-10 inline-flex h-fit w-fit font-medium" href={path}>
-      <div className="bg-gradient-primary absolute -left-[1px] -top-[1px] -z-10 hidden h-[calc(100%+2px)] w-[calc(100%+2px)] rounded-[10px] border-solid opacity-0 transition-all group-hover:opacity-100 md:inline-block" />
+    <Link
+      className={clsx(
+        'group relative z-10 inline-flex h-fit w-fit font-medium text-text-secondary transition-colors hover:text-primary-200',
+        {
+          'text-primary-200': isActive
+        }
+      )}
+      href={path}
+    >
+      <div className="absolute -left-[1px] -top-[1px] -z-10 hidden h-[calc(100%+2px)] w-[calc(100%+2px)] rounded-[10px] border-solid bg-gradient-primary opacity-0 transition-all group-hover:opacity-100 md:inline-block" />
       <p className="md:rounded-[10px] md:bg-black md:px-[14px] md:py-[9px]">{children || label}</p>
     </Link>
   )
