b533942c9468e79d6e16c4041209f038b043d5a8
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 18:46:18 -0500

    refactor: simplify middleware logic for authentication and session management

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,24 @@
-import { updateSession } from '@/modules/core/utils/supabase/middleware'
-import { NextResponse, type NextRequest } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
-import { createClientServer } from './modules/core/utils/supabase/create-client-server'
+import { env } from './modules/core/lib/env'
+import { updateSession } from './modules/core/utils/supabase/middleware'
 
-/**
- * Middleware function to handle authentication and session management.
- *
- * @param {NextRequest} request - The incoming request object.
- * @returns {Promise<NextResponse>} - The response object after processing the request.
- *
- * This middleware performs the following tasks:
- * 1. Updates the session using the `updateSession` function.
- * 2. Creates a Supabase client on the server side.
- * 3. Retrieves the current session from Supabase authentication.
- * 4. Checks the request URL pathname and session status to enforce authentication:
- *    - Redirects to the login page if accessing `/admin` without a session.
- *    - Returns a 401 Unauthorized response if accessing non-GET `/api` endpoints without a session.
- * 5. Returns the response from the `updateSession` function if no redirection or unauthorized access occurs.
- */
 export async function middleware(request: NextRequest) {
-  const supabaseResponse = await updateSession(request)
-
-  const supabase = await createClientServer()
-  const {
-    data: { session }
-  } = await supabase.auth.getSession()
-
   const { pathname } = request.nextUrl
 
-  if (pathname.startsWith('/admin') && !session) {
-    const loginUrl = new URL('/login', request.url)
-    loginUrl.searchParams.set('redirect', pathname)
+  if (pathname.startsWith('/api')) {
+    const apiToken = request.headers.get('authorization')
 
-    return NextResponse.redirect(loginUrl)
+    if (!apiToken || apiToken !== env.SECRET_KEY) {
+      return NextResponse.json({ error: 'No autorizado' }, { status: 401 })
+    }
   }
 
-  if (pathname.startsWith('/api') && request.method !== 'GET' && !session) {
-    return NextResponse.json({ message: 'No autorizado' }, { status: 401 })
+  if (pathname.startsWith('/admin')) {
+    return await updateSession(request)
   }
 
-  return supabaseResponse
+  return NextResponse.next()
 }
 
 export const config = {
