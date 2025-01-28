487a48493ae862316abd9db494cc386833e09af4
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 18:46:31 -0500

    feat: replace static comments section with dynamic CommentsContainer and improve loading state

--- src/app/(users-layout)/page.tsx
+++ src/app/(users-layout)/page.tsx
@@ -1,19 +1,21 @@
+import { Comments } from '@/modules/home/components/comments/comments'
+import { CommentsContainer } from '@/modules/home/components/comments/comments-container'
+import { CommentsHeader } from '@/modules/home/components/comments/comments-header'
 import { Header } from '@/modules/home/components/header/header'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <>
       <Header className="mb-28 mt-40 md:mb-0 md:mt-14" />
 
-      <div className="border-t border-border-default bg-bg-muted py-10">
-        <p className="px-3 text-center text-sm font-medium text-text-secondary md:text-base">
-          Muchos de nuestros clientes y fans que{' '}
-          <span className="text-text-primary">confían en nosotros</span> dejaron su comentario.
-        </p>
-        <p className="mt-0.5 px-3 text-center text-sm font-medium text-text-secondary md:text-base">
-          Deja el tuyo dando <span className="font-bold text-primary-100">clic aquí.</span>
-        </p>
-      </div>
+      <CommentsContainer>
+        <CommentsHeader />
+
+        <Suspense fallback={<div className="h-52" />}>
+          <Comments className="mt-5" />
+        </Suspense>
+      </CommentsContainer>
     </>
   )
 }
