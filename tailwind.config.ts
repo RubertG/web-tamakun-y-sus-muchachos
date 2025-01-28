a81063bc314b372c256078321adf7d09710f309f
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 16:25:08 -0500

    feat: add support for Tailwind CSS with animations and create Marquee components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -25,7 +27,7 @@ export default {
         'primary-200': '#e50101',
         'primary-300': '#c3140f',
         'bg-dark': '#060606',
-        'bg-muted': '#0a0909',
+        'bg-muted': '#100E0E',
         'bg-accent': '#302f32',
         'accent-100': '#1c53f9',
         'accent-200': '#1544d0',
@@ -34,9 +36,79 @@ export default {
         'text-secondary': '#c6c6c6',
         'text-muted': '#939393',
         'text-inverse': '#1f1f1f',
-        'border-default': '#1a1a1a'
+        'border-default': '#1E1E1E',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: 'translateX(0)'
+          },
+          to: {
+            transform:
+              'translateX(calc(-100% - var(--gap)))'
+          }
+        },
+        'marquee-vertical': {
+          from: {
+            transform: 'translateY(0)'
+          },
+          to: {
+            transform:
+              'translateY(calc(-100% - var(--gap)))'
+          }
+        }
+      },
+      animation: {
+        marquee: 'marquee var(--duration) infinite linear',
+        'marquee-vertical':
+          'marquee-vertical var(--duration) linear infinite'
       }
     }
   },
-  plugins: []
+  plugins: [tailwindcssAnimate]
 } satisfies Config
