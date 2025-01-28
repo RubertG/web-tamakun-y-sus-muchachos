f7674990bb7d073e4467f273700875b45a7ba9fc
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 18:47:00 -0500

    feat: agregar rutas de API para gestionar comentarios con parámetros dinámicos

---
+++ src/modules/core/utils/api/apiRoutes.ts
@@ -0,0 +1,15 @@
+import { env } from '../../lib/env'
+
+export const apiRoutes = {
+  comments: {
+    allComments: (start?: number, end?: number, areActives?: boolean) => {
+      const urlSearchParams = new URLSearchParams({
+        ...(start !== undefined && { start: start.toString() }),
+        ...(end !== undefined && { end: end.toString() }),
+        ...(areActives !== undefined && { areActives: areActives.toString() })
+      }).toString()
+
+      return `${env.NEXT_PUBLIC_DOMAIN}/api/comments?${urlSearchParams}`
+    }
+  }
+}
