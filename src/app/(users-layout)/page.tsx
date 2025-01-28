8b0cc2036cbe837d078ce7d4b54691ec24a0ad84
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 13:37:11 -0500

    feat: add Header component and customer feedback section to Home page

--- src/app/(users-layout)/page.tsx
+++ src/app/(users-layout)/page.tsx
@@ -1,3 +1,25 @@
+import { Header } from '@/modules/home/components/header/header'
+
 export default function Home() {
-  return <div>Hola mundo</div>
+  return (
+    <>
+      <Header className="mb-28 mt-40 md:mb-0 md:mt-14" />
+
+      <div className="border-t border-border-default bg-bg-muted py-10">
+        <p className="px-3 text-center text-sm font-medium text-text-secondary md:text-base">
+          Muchos de nuestros clientes y fans que{' '}
+          <span className="text-text-primary">
+            confían en nosotros
+          </span>{' '}
+          dejaron su comentario.
+        </p>
+        <p className="mt-0.5 px-3 text-center text-sm font-medium text-text-secondary md:text-base">
+          Deja el tuyo dando{' '}
+          <span className="font-bold text-primary-100">
+            clic aquí.
+          </span>
+        </p>
+      </div>
+    </>
+  )
 }
