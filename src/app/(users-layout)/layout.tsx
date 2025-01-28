4e71f1a94bce932f7fb75b6a0c62e972946cc3e6
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 13:07:46 -0500

    feat: add decoration component to PublicLayout

--- src/app/(users-layout)/layout.tsx
+++ src/app/(users-layout)/layout.tsx
@@ -1,9 +1,11 @@
 import { UsersNav } from '@/modules/core/components/nav/users-nav'
+import { Decoration } from '@/modules/core/ui/decoration'
 import { PropsWithChildren } from 'react'
 
 function PublicLayout({ children }: PropsWithChildren) {
   return (
     <>
+      <Decoration className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/2" />
       <UsersNav />
       {children}
     </>
