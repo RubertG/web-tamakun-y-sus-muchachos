3d2623ba24e65d8916503082d8b50aa55053ae66
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-01-28 13:36:38 -0500

    feat: add responsive text gradient styles and custom container class

--- src/modules/core/components/nav/nav-item.tsx
+++ src/modules/core/components/nav/nav-item.tsx
@@ -29,7 +29,8 @@ const NavItem = ({
         className={clsx(
           'capitalize md:rounded-[10px] md:px-[14px] md:py-[9px] md:transition-colors md:duration-200 md:group-hover:bg-bg-dark md:group-hover:text-primary-200 md:group-hover:duration-100',
           {
-            'text-primary-200': isActive,
+            'text-gradient-primary text-gradient-primary-off':
+              isActive,
             'text-text-secondary': !isActive
           }
         )}
