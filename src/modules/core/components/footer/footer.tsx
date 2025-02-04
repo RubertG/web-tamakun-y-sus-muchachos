af6f8d670031a48989551e2603b26b2e126d2701
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-04 16:28:09 -0500

    feat: add footer credit with link to contact page

--- src/modules/core/components/footer/footer.tsx
+++ src/modules/core/components/footer/footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Decoration } from '../../ui/decoration'
 
 interface Props {
@@ -6,11 +8,18 @@ interface Props {
 
 const Footer = ({ className }: Props) => {
   return (
-    <footer className={`relative h-0 ${className}`}>
+    <footer className={`relative pb-6 ${className}`}>
       <Decoration
         className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 overflow-hidden"
         variant="secondary"
       />
+
+      <p className="text-center text-sm text-gray-500">
+        Diseñado y desarrollado por{' '}
+        <Link href="/contacto" className="font-bold hover:underline">
+          WebMinds Colombia
+        </Link>
+      </p>
     </footer>
   )
 }
