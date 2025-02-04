229ba57fa082027029b757f572a0d6596cc10b55
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-04 15:57:26 -0500

    feat: enhance EventPage layout with loading skeletons and contact button

--- src/app/(users-layout)/eventos/page.tsx
+++ src/app/(users-layout)/eventos/page.tsx
@@ -1,3 +1,36 @@
+import { SocialIcons } from '@/modules/contact/components/social-icons'
+import { Button } from '@/modules/core/ui/button'
+import { EventCardSkeleton } from '@/modules/events/components/event-card-skeleton'
+import { Events } from '@/modules/home/components/events/events'
+import Link from 'next/link'
+import { Suspense } from 'react'
+
 export default function EventPage() {
-  return <div>Página de los eventos</div>
+  return (
+    <div className="flex min-h-dvh flex-col items-center justify-center px-3 py-24">
+      <h1 className="px-3 text-center text-4xl font-bold md:text-6xl">Próximas fechas</h1>
+
+      <section className="mx-auto max-w-md">
+        <Suspense
+          fallback={
+            <ul className="mt-12 space-y-6">
+              {[1, 2, 3, 4].map((key) => (
+                <EventCardSkeleton key={key} />
+              ))}
+            </ul>
+          }
+        >
+          <Events className="mt-9" />
+        </Suspense>
+
+        <Link href="/contacto" className="mt-9 block">
+          <Button className="w-full" variant="secondary">
+            ¡Contáctanos!
+          </Button>
+        </Link>
+
+        <SocialIcons className="mt-8" />
+      </section>
+    </div>
+  )
 }
