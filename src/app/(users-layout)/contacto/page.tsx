3b4928fa882cce10f9ca0fa83f6638f37e50b581
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-05 17:39:42 -0500

    feat: implement ContactPage with ContactHeader component

--- src/app/(users-layout)/contacto/page.tsx
+++ src/app/(users-layout)/contacto/page.tsx
@@ -1,3 +1,9 @@
+import { ContactHeader } from '@/modules/contact/components/contact-header'
+
 export default function ContactPage() {
-  return <div>Página de contacto</div>
+  return (
+    <div className="min-h-[calc(100vh-44px)] px-3 pb-16 pt-24">
+      <ContactHeader className="mx-auto max-w-3xl" />
+    </div>
+  )
 }
