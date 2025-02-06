7bed81e1a1da25400802315f8c41f39976260768
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-06 16:04:39 -0500

    feat: refactor EventPage to use PageContainer and create EventsSection component

--- src/app/(users-layout)/eventos/page.tsx
+++ src/app/(users-layout)/eventos/page.tsx
@@ -1,43 +1,19 @@
-import { SocialIcons } from '@/modules/contact/components/social/social-icons'
-import { Button } from '@/modules/core/ui/button'
-import { EventCardSkeleton } from '@/modules/events/components/event-card-skeleton'
-import { Events } from '@/modules/home/components/events/events'
-import Link from 'next/link'
-import { Suspense } from 'react'
+import { PageContainer } from '@/modules/core/components/layouts/page-container'
+import { EventsSection } from '@/modules/events/components/events-section'
 
 export default function EventPage() {
   return (
-    <div className="min-h-[calc(100vh-44px)] px-3 pb-16 pt-24 md:flex md:flex-col md:items-center md:justify-center">
+    <PageContainer>
       <h1 className="animation-entry text-center text-4xl font-bold md:text-6xl">
         Próximas fechas
       </h1>
 
-      <section
-        className="animation-entry mx-auto max-w-md"
+      <EventsSection
+        className="animation-entry"
         style={{
           animationDelay: '0.15s'
         }}
-      >
-        <Suspense
-          fallback={
-            <ul className="mt-12 space-y-6">
-              {[1, 2, 3, 4].map((key) => (
-                <EventCardSkeleton key={key} />
-              ))}
-            </ul>
-          }
-        >
-          <Events className="mt-9" />
-        </Suspense>
-
-        <Link href="/contacto" className="mt-9 block">
-          <Button className="w-full" variant="secondary" size="small">
-            ¡Contáctanos!
-          </Button>
-        </Link>
-
-        <SocialIcons className="mt-8" />
-      </section>
-    </div>
+      />
+    </PageContainer>
   )
 }
---
+++ src/modules/events/components/events-section.tsx
@@ -0,0 +1,35 @@
+import { SocialIcons } from '@/modules/contact/components/social/social-icons'
+import { Button } from '@/modules/core/ui/button'
+import { Events } from '@/modules/home/components/events/events'
+import Link from 'next/link'
+import { Suspense } from 'react'
+
+import { EventCardSkeleton } from './event-card-skeleton'
+
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+export const EventsSection = ({ className, ...props }: Props) => {
+  return (
+    <section className={`mx-auto max-w-md ${className}`} {...props}>
+      <Suspense
+        fallback={
+          <ul className="mt-12 space-y-6">
+            {[1, 2, 3, 4].map((key) => (
+              <EventCardSkeleton key={key} />
+            ))}
+          </ul>
+        }
+      >
+        <Events className="mt-9" />
+      </Suspense>
+
+      <Link href="/contacto" className="mt-9 block">
+        <Button className="w-full" variant="secondary" size="small">
+          ¡Contáctanos!
+        </Button>
+      </Link>
+
+      <SocialIcons className="mt-8" />
+    </section>
+  )
+}
