d0b96d805c7c5066f917e4b1a76607db0dea5a95
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-06 15:36:20 -0500

    feat: add Comments component to display active comments

---
+++ src/modules/contact/components/comments/comments.tsx
@@ -0,0 +1,26 @@
+import { Comment } from '@/modules/core/components/comments/comment'
+import { getComments } from '@/modules/core/services/comments'
+
+interface Props {
+  className?: string
+}
+
+const Comments = async ({ className }: Props) => {
+  const { data } = await getComments({ areActives: true })
+
+  return (
+    <ul
+      className={`grid grid-cols-1 justify-center gap-x-4 gap-y-3 md:grid-cols-2 lg:grid-cols-3 ${className}`}
+    >
+      {data?.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment}
+          className="mx-auto bg-bg-dark/20 backdrop-blur-md hover:bg-bg-dark"
+        />
+      ))}
+    </ul>
+  )
+}
+
+export { Comments }
