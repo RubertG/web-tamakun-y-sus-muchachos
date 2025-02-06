a83eaba686406590209b1431813c0e51cdfcc36c
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-06 15:36:23 -0500

    feat: integrate Comments and CommentsHeader components into ContactPage

--- src/app/(users-layout)/contacto/page.tsx
+++ src/app/(users-layout)/contacto/page.tsx
@@ -1,9 +1,30 @@
-import { ContactHeader } from '@/modules/contact/components/contact-header'
+import { Comments } from '@/modules/contact/components/comments/comments'
+import { ContactHeader } from '@/modules/contact/components/header/contact-header'
+import { Loader } from '@/modules/core/ui/loader'
+import { CommentsHeader } from '@/modules/home/components/comments/comments-header'
+import { Suspense } from 'react'
 
 export default function ContactPage() {
   return (
-    <div className="min-h-[calc(100vh-44px)] px-3 pb-16 pt-24">
+    <div className="min-h-[calc(100vh-44px)] px-3 pb-16 pt-24 md:flex md:flex-col md:items-center md:justify-center">
       <ContactHeader className="mx-auto max-w-3xl" />
+
+      <section
+        className="animation-entry mx-auto mt-14 max-w-7xl space-y-6"
+        style={{ animationDelay: '0.45s' }}
+      >
+        <CommentsHeader />
+
+        <Suspense
+          fallback={
+            <div className="grid h-52 place-content-center">
+              <Loader />
+            </div>
+          }
+        >
+          <Comments />
+        </Suspense>
+      </section>
     </div>
   )
 }
