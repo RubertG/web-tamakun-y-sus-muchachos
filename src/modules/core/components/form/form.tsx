609d9226530abe5bfa93a26655f5119bcd4955cb
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-06 19:10:11 -0500

    feat: add Form component with custom styles and props forwarding

---
+++ src/modules/core/components/form/form.tsx
@@ -0,0 +1,12 @@
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+const Form = ({ className, ...props }: Props) => {
+  return (
+    <div
+      className={`w-full max-w-lg rounded-[10px] border border-border-default bg-bg-dark/20 p-4 backdrop-blur-md md:p-6 ${className}`}
+      {...props}
+    ></div>
+  )
+}
+
+export default Form
