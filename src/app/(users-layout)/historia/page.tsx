680e4bd307bebe48ca95c0047871868038690a91
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-06 19:09:38 -0500

    feat: update GaleryPage and HistoryPage to center content and adjust padding in PageContainer

--- src/app/(users-layout)/historia/page.tsx
+++ src/app/(users-layout)/historia/page.tsx
@@ -3,7 +3,7 @@ import { PageContainer } from '@/modules/core/components/layouts/page-container'
 
 export default function HistoryPage() {
   return (
-    <PageContainer>
+    <PageContainer className="flex items-center justify-center !pt-0 md:!pt-24">
       <ComingSoon />
     </PageContainer>
   )
