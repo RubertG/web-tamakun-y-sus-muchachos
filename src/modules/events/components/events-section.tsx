8d44cf1859b7ef5baaab9ea7c8955656200ae173
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-07 12:55:16 -0500

    feat: adjust event section styles to full width and modify loading skeleton spacing

--- src/modules/events/components/events-section.tsx
+++ src/modules/events/components/events-section.tsx
@@ -10,10 +10,10 @@ type Props = React.HTMLAttributes<HTMLDivElement>
 
 export const EventsSection = ({ className, ...props }: Props) => {
   return (
-    <section className={`mx-auto max-w-md ${className}`} {...props}>
+    <section className={`mx-auto w-full max-w-md ${className}`} {...props}>
       <Suspense
         fallback={
-          <ul className="mt-12 space-y-6">
+          <ul className="mt-9 space-y-6">
             {[1, 2, 3, 4].map((key) => (
               <EventCardSkeleton key={key} />
             ))}
