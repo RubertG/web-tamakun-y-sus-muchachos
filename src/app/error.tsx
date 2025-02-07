0d56f58d836caed1d39ce18eaf110515907ee433
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-02-07 13:37:40 -0500

    feat: error handling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,21 @@
 'use client'
 
-export default function ErrorPage() {
-  return <div>404 - Página no encontrada</div>
+import { Button } from '@/modules/core/ui/button'
+
+interface Props {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+export default function Error({ reset }: Props) {
+  return (
+    <main className="flex h-dvh flex-col items-center justify-center space-y-4">
+      <h2 className="text-8xl font-bold">Ops</h2>
+
+      <p className="mt-2 text-sm text-text-secondary md:text-base">Ocurrió un error inesperado.</p>
+      <Button variant="secondary" size="small" onClick={() => reset()}>
+        Volver a intentar
+      </Button>
+    </main>
+  )
 }
